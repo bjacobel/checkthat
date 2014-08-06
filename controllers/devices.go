767601a95cfcdd56941ba63f8c04b9ea2229cf7b
Brian Jacobel <bjacobel@gmail.com>
2014-08-06 15:42:41 -0400

    Joins

--- controllers/devices.go
+++ controllers/devices.go
@@ -16,6 +16,21 @@ type DeviceController struct {
 	db gorm.DB
 }
 
+type JoinedResult struct {
+   	DeviceId			int64
+   	DeviceOs			string
+   	DeviceType			string
+   	DeviceName			string
+   	DeviceVersion		float32
+   	DeviceNfcSerial		int64
+   	DeviceCheckedOut	int64
+   	UserId				int64
+   	UserNfcSerial		int64
+   	UserFirstName		string
+   	UserLastName		string
+   	UserTel				string
+}
+
 func NewDeviceController() *DeviceController {
 	output := new(DeviceController)
 
@@ -32,13 +47,25 @@ func NewDeviceController() *DeviceController {
 }
 
 func (this *DeviceController) Get(ctx *ripple.Context) {
-    //this.db.Table("users").Select("users.name, emails.email").Joins("left join emails on emails.user_id = users.id").Scan(&results)
-
-
 	deviceId, _ := strconv.Atoi(ctx.Params["id"])
 
+    scan := JoinedResult{}
+
 	listResponse := map[string]*gorm.DB{}
-	listResponse["checked_out"] = this.db.Where("user_id >= ?", 2).Find(&[]models.Device{})
+	listResponse["checked_out"] = this.db.Table("devices").Joins("left join users on users.id = devices.user_id").Select(`
+    	devices.id as device_id,
+    	devices.os as device_os,
+    	devices.type as device_type,
+    	devices.name as device_name,
+    	devices.version as device_version,
+    	devices.nfc_serial as device_nfc_serial,
+    	devices.checked_out as device_checked_out,
+    	users.id as user_id,
+    	users.nfc_serial as user_nfc_serial,
+    	users.first_name as user_first_name,
+    	users.last_name as user_last_name,
+    	users.tel as user_tel
+    `).Where("user_id >= ?", 2).Find(&scan)
 	listResponse["checked_in"] = this.db.Where("user_id = ?", 1).Find(&[]models.Device{})
 
 	if deviceId > 0 {
