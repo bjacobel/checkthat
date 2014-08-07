3be881dad93d9bf52f4897a46d029904e3c2c073
Brian Jacobel <bjacobel@gmail.com>
2014-08-07 10:04:37 -0400

    Field naming standardized, fixed some bugs

--- controllers/devices.go
+++ controllers/devices.go
@@ -17,24 +17,25 @@ type DeviceController struct {
 }
 
 type JoinedResult struct {
-	DeviceId 			int64
-	DeviceOs 			string
-	DeviceType 			string
-	DeviceName 			string
-	DeviceVersion		float32
-	DeviceNfcSerial		int64
-	DeviceCheckedOut 	int64
-	UserId 				int64
-	UserNfcSerial 		int64
-	UserFirstName		string
-	UserLastName 		string
-	UserTel				string
+	Id 				int64
+	Os 				string
+	Type 			string
+	Name 			string
+	Version			float32
+	NfcSerial		int64
+	CheckedOut 		int64
+	UserId 			int64
+	UserNfcSerial 	int64
+	UserFirstName	string
+	UserLastName 	string
+	UserTel			string
 }
 
 func NewDeviceController() *DeviceController {
 	output := new(DeviceController)
 
 	db, dberr := gorm.Open("postgres", fmt.Sprintf("postgres://%s:%s@ec2-54-197-241-67.compute-1.amazonaws.com:5432/%s", os.Getenv("PGUSER"), os.Getenv("PGPW"), os.Getenv("PGDB")))
+	
 	if dberr != nil {
 		panic(dberr)
 	}
@@ -49,24 +50,26 @@ func NewDeviceController() *DeviceController {
 func (this *DeviceController) Get(ctx *ripple.Context) {
 	deviceId, _ := strconv.Atoi(ctx.Params["id"])
 
-	scan := JoinedResult{}
+	scan := []JoinedResult{}
 
 	listResponse := map[string]*gorm.DB{}
+
 	listResponse["checked_out"] = this.db.Table("devices").Joins("left join users on users.id = devices.user_id").Select(`
-		devices.id as device_id,
-		devices.os as device_os,
-		devices.type as device_type,
-		devices.name as device_name,
-		devices.version as device_version,
-		devices.nfc_serial as device_nfc_serial,
-		devices.checked_out as device_checked_out,
+		devices.id as id,
+		devices.os as os,
+		devices.type as type,
+		devices.name as name,
+		devices.version as version,
+		devices.nfc_serial as nfc_serial,
+		devices.checked_out as checked_out,
 		users.id as user_id,
 		users.nfc_serial as user_nfc_serial,
 		users.first_name as user_first_name,
 		users.last_name as user_last_name,
 		users.tel as user_tel
-	`).Where("user_id >= ?", 2).Find(&scan)
-	listResponse["checked_in"] = this.db.Where("user_id = ?", 1).Find(&[]models.Device{})
+	`).Where("user_id >= ?", 1).Find(&scan)
+
+	listResponse["checked_in"] = this.db.Where("user_id = ?", 0).Find(&[]models.Device{})
 
 	if deviceId > 0 {
 		ctx.Response.Body = this.db.First(&models.Device{}, deviceId)
