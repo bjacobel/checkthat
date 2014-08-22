8de75e4b54995a3cb50c15052502c3e6ac8543ab
Brian Jacobel <bjacobel@gmail.com>
2014-08-22 15:54:42 -0400

    Updating static files

--- angular/app/scripts/controllers/devices_controller.js
+++ angular/app/scripts/controllers/devices_controller.js
@@ -7,13 +7,13 @@ angular.module('CheckCMS')
         DevicesService.getDeviceList(function(data){
             $scope.devices = data.CheckedOut.concat(data.CheckedIn);
 
-            $scope.devices.map(function(curr, index, array){
-                if (data.CheckedOut.indexOf(curr) != -1){
-                    curr.status = "out";
-                } else if (data.CheckedIn.indexOf(curr) != -1){
-                    curr.status = "in";
+            $scope.devices.map(function(curr){
+                if (data.CheckedOut.indexOf(curr) !== -1){
+                    curr.status = 'out';
+                } else if (data.CheckedIn.indexOf(curr) !== -1){
+                    curr.status = 'in';
                 }
                 return curr;
-            })
+            });
         });
     }]);
